6cadc77b7e003d10513659e342419ef6114cafe7
SatvikDeveloper <satvikbhungaliya.r@gmail.com>
2023-08-18 17:36:37 +0530

    Employee Flow Completed

---
+++ lib/core/constants/api_url.dart
@@ -0,0 +1,9 @@
+class ApiUrl {
+
+  static String liveBaseURL = 'http://ec2-65-2-182-34.ap-south-1.compute.amazonaws.com:3000';
+
+    //========================AUTH============================
+
+  static String signUp = liveBaseURL + '/owner/ownerRegister';
+
+}
